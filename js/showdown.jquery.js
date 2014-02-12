3ddb160b86c80d18034d2467a037ea3683ad8e29
Imanuel Ulbricht <knerdprod@live.de>
2014-02-12 17:41:14 +0100

    task viewer

---
+++ js/showdown.jquery.js
@@ -0,0 +1,18 @@
+﻿$.fn.makeHtml = function (options) {
+    var converter = new Showdown.converter({ extensions: ['github', 'prettify', 'table'] });
+    var html = converter.makeHtml($(this).text());
+    if (options && options.output) {
+        $(options.output).html(html);
+    } else {
+        $(this).html(html);
+    }
+}
+$(document).ready(function () {
+    $('*[data-markdown=true]').each(function () {
+        var options = {};
+        if ($(this).data('target')) {
+            options.output = '#' + $(this).data('target');
+        }
+        $(this).makeHtml(options);
+    });
+});
